cdab72c8376adb38230e1f716657d73b2a415b12
Sosa Nico <140737842+Sosapapu@users.noreply.github.com>
2023-08-10 12:13:30 -0300

    Add files via upload

---
+++ JAVA.JS
@@ -0,0 +1,24 @@
+var entradaTarea = document.getElementById("entradaTarea");
+var listaTareas = document.getElementById("listaTareas");
+
+function agregarTarea() {
+  var textoTarea = entradaTarea.value;
+
+  if (textoTarea !== "") {
+    var elementoTarea = document.createElement("li");
+    elementoTarea.textContent = textoTarea;
+
+    var botonEliminar = document.createElement("button");
+    botonEliminar.textContent = "Eliminar";
+    botonEliminar.classList.add("delete-button");
+    elementoTarea.appendChild(botonEliminar);
+
+    botonEliminar.addEventListener("click", function () {
+      elementoTarea.remove();
+    });
+
+    listaTareas.appendChild(elementoTarea);
+
+    entradaTarea.value = "";
+  }
+}
